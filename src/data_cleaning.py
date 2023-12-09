ef57ec369fe481453695fc4b21038b453ea2799c
TonBorrell <tonborrell@gmail.com>
2023-12-09 19:58:41 +0100

    Add more columns + use lightgbm

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -23,6 +23,18 @@ def data_cleaning(raw_df: pd.DataFrame, logger) -> pd.DataFrame:
         "dif_discount_prev_month",
         "dif_discount_prev_2_month",
         "dif_discount_prev_3_month",
+        "service_mobile_pending_install",
+        "service_fix_pending_install",
+        "service_mobile_cancelled",
+        "service_fix_cancelled",
+        "service_mobile_pending_install_3month",
+        "service_fix_pending_install_3month",
+        "service_mobile_cancelled_3month",
+        "service_fix_cancelled_3month",
+        "service_mobile_pending_install_6month",
+        "service_fix_pending_install_6month",
+        "service_mobile_cancelled_6month",
+        "service_fix_cancelled_6month",
     ]
 
     # to-be-cooked features
@@ -32,13 +44,46 @@ def data_cleaning(raw_df: pd.DataFrame, logger) -> pd.DataFrame:
         "aperiodica_0",
         "discount_0",
         "ajuste_0",
+        "NUM_GB_OWNN_CURR",
+        "NUM_GB_2G_CURR",
+        "NUM_GB_3G_CURR",
+        "NUM_GB_4G_CURR",
+        "NUM_GB_5G_CURR",
+        "NUM_SESS_CURR",
+        "NUM_SECS_CURR",
+        "NUM_CALL_CURR",
+        "NUM_CALL_WEEK_CURR",
+        "NUM_CALL_WEEKEND_CURR",
+        "NUM_SECS_WEEK_CURR",
+        "NUM_SECS_WEEKEND_CURR",
+        "NUM_CALL_WEEK",
+        "NUM_CALL_WEEKEND",
+    ]
+
+    # direct-to-model features
+    keep_cols = [
+        "NUM_DAYS_ACT",
+        "order_mobile_from_new_alta",
+        "MIN_DAYS_PERM_CURR",
+        "PREV_FINISHED_PERM",
     ]
 
     # target
     target_col = ["NUM_DAYS_LINE_TYPE_FIXE_POST_DEA"]
 
-    filter_df = raw_df[users_cols + diff_cols + transform_cols + target_col]
+    filter_df = raw_df[users_cols + diff_cols + keep_cols + transform_cols + target_col]
     clean_df = filter_df.dropna(how="all")
+
+    columns_to_drop = []
+    for col in clean_df.columns:
+        number_nulls = clean_df[col].isnull().sum()
+        if number_nulls > len(clean_df) // 2 and col != target_col[0]:
+            columns_to_drop.append(col)
+            logger.info(f"Dropping column {col}")
+        elif number_nulls > 0:
+            clean_df[col] = clean_df[col].fillna(value=0)
+
+    clean_df = clean_df.drop(columns=columns_to_drop)
 
     logger.info("Completed cleaning data!")
     logger.info(clean_df.head())
